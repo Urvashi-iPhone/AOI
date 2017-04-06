673ce8d5ff365aeb4235fdd7e19252ef6ec770b8
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-04-06 10:39:20 +0530

    Initial Commit

---
+++ AOI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AOI
+//
+//  Created by Tecksky Techonologies on 4/6/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AOI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AOI
+//
+//  Created by Tecksky Techonologies on 4/6/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
